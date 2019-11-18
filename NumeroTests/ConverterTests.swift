60bdc48264497e5f2efb1fbec0403b55f65743af
Naveen <naveen.singisetty@xinthe.com>
2019-11-18 12:39:06 +0530

    added travis.yml file

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -32,14 +32,16 @@ import XCTest
 
 class ConverterTests: XCTestCase {
   
-  let converter = Converter()
+    var converter : Converter!
   
   override func setUp() {
     super.setUp()
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    converter = Converter()
   }
   
   override func tearDown() {
+    converter = nil
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     super.tearDown()
   }
